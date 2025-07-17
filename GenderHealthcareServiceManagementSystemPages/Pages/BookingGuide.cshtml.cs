22d29fbf4f9bb0d131881e8e816fe44d222214d0
Nguyen Minh Trung <nguyenminhtrung18072004@gmail.com>
2025-07-17 23:52:49 +0700

    Add: Booking Guide page and update UI for Profile page

---
+++ GenderHealthcareServiceManagementSystemPages/Pages/BookingGuide.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GenderHealthcareServiceManagementSystemPages.Pages
+{
+    public class BookingGuideModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
