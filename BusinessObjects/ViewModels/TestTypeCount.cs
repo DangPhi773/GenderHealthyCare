356de70c61c3f6a1d9c7866efb50cdda8b65f2d5
0767534208 <quanghuy010503@gmail.com>
2025-08-21 07:14:41 +0700

    Thống kê : số lượng xét nghiệm, số loại xét nghiệm, từng loại xét nghiệm với bác sĩ phụ trách

---
+++ BusinessObjects/ViewModels/TestTypeCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.ViewModels
+{
+    public class TestTypeCount
+    {
+        public string ServiceName { get; set; } = string.Empty;
+        public int Count { get; set; }
+    }
+}
